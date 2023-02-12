253c0efe0efe876cd51bd744f82c40a1aae7a598
bolee <1160360517@qq.com>
2023-02-12 14:28:56 +0800

    gitlet

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -9,66 +9,70 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         int argNum = args.length;
         if(argNum == 0) Utils.exitWithError("Please enter a command.");
         String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                Repository.repositorySetup();
-                break;
-            case "add":
-                // handle the `add [filename]` command
-                String filePath = args[1];
-                Repository.addFile(filePath);
-                break;
-            case "commit":
-                String message = args[1];
-                Repository.commit(message);
-                break;
-            case "rm":
-                Repository.rmFile(args[1]);
-                break;
-            case "log":
-                Repository.printLog();
-                break;
-            case "global-log":
-                Repository.printGlobalLog();
-                break;
-            case "find":
-                Repository.find(args[1]);
-                break;
-            case "status":
-                Repository.printStatus();
-                break;
-            case "checkout":
-                String secondArg = args[1];
-                if(argNum == 2){
-                    Repository.checkoutFromBranch(secondArg);
-                }else if(argNum == 3 && secondArg.equals("--")) {
-                    Repository.checkoutTheFileFromLastestCommit(args[2]);
-                }else if(argNum == 4 && args[2].equals("--")){
-                    String forthArg = args[3];
-                    Repository.checkoutTheFileFromCommitOf(secondArg,forthArg);
-                }else{
-                    Utils.exitWithError("Incorrect operands.");
-                }
-                break;
-            case "branch":
-                Repository.createBranch(args[1]);
-                break;
-            case "rm-branch":
-                Repository.removeBranch(args[1]);
-                break;
-            case "reset":
-                Repository.resetToCommit(args[1]);
-                break;
-            case "merge":
-                Repository.merge(args[1]);
-                break;
-            default:
-                Utils.exitWithError("No command with that name exists.");
-                break;
+        try {
+            switch(firstArg) {
+                case "init":
+                    Repository.repositorySetup();
+                    break;
+                case "add":
+                    // handle the `add [filename]` command
+                    String filePath = args[1];
+                    Repository.addFile(filePath);
+                    break;
+                case "commit":
+                    String message = args[1];
+                    Repository.commit(message);
+                    break;
+                case "rm":
+                    Repository.rmFile(args[1]);
+                    break;
+                case "log":
+                    Repository.printLog();
+                    break;
+                case "global-log":
+                    Repository.printGlobalLog();
+                    break;
+                case "find":
+                    Repository.find(args[1]);
+                    break;
+                case "status":
+                    Repository.printStatus();
+                    break;
+                case "checkout":
+                    String secondArg = args[1];
+                    if(argNum == 2){
+                        Repository.checkoutFromBranch(secondArg);
+                    }else if(argNum == 3 && secondArg.equals("--")) {
+                        Repository.checkoutTheFileFromLastestCommit(args[2]);
+                    }else if(argNum == 4 && args[2].equals("--")){
+                        String forthArg = args[3];
+                        Repository.checkoutTheFileFromCommitOf(secondArg,forthArg);
+                    }else{
+                        Utils.exitWithError("Incorrect operands.");
+                    }
+                    break;
+                case "branch":
+                    Repository.createBranch(args[1]);
+                    break;
+                case "rm-branch":
+                    Repository.removeBranch(args[1]);
+                    break;
+                case "reset":
+                    Repository.resetToCommit(args[1]);
+                    break;
+                case "merge":
+                    Repository.merge(args[1]);
+                    break;
+                default:
+                    Utils.exitWithError("No command with that name exists.");
+                    break;
+        }
+        }catch (IOException exception){
+            System.out.println("fuckyou");
         }
     }
 }
